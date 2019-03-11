88c0f56b128c5133bf8ec80b77d5a57ed256400a
Anders <Anders2605-94@live.dk>
2019-03-11 15:44:05 +0100

    Added comments to most of the code

--- app/src/main/java/com/example/Booga/LoginActivity.java
+++ app/src/main/java/com/example/Booga/LoginActivity.java
@@ -28,6 +28,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         mAuth = FirebaseAuth.getInstance();
 
+
         LoginProgressBar =  findViewById(R.id.loginProgressBarId);
         loginEditTextEmail = findViewById(R.id.editTextEmailId);
         loginEditTextPassword = findViewById(R.id.editTextPasswordId);
@@ -36,29 +37,42 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.buttonLoginId).setOnClickListener(this);
     }
 
+    /*
+    This method is used to login the user
+     */
     private void userLogin() {
 
+        /*
+        This gets the text written by the user in the EditTextFields email and password.
+         */
         String email = loginEditTextEmail.getText().toString().trim();
         String password = loginEditTextPassword.getText().toString().trim();
 
+        // If the Email EditTextField is empty show an error and point where the error is by using .requestFocus
         if (email.isEmpty()) {
             loginEditTextEmail.setError("Email is required");
             loginEditTextEmail.requestFocus();
             return;
         }
 
+        /*
+         If the Email in the EditTextField does not match with an already registered Email in Firebase,
+         show an error and point where the error is by using .requestFocus
+         */
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             loginEditTextEmail.setError("Please enter a valid email");
             loginEditTextEmail.requestFocus();
             return;
         }
 
+        // If the Password EditTextField is empty show an error and point where the error is by using .requestFocus
         if(password.isEmpty()) {
             loginEditTextPassword.setError("Password is required");
             loginEditTextPassword.requestFocus();
             return;
         }
 
+        // If the Password EditTextField is not long enough, show an error and point where the error is by using .requestFocus
         if(password.length() < 6) {
             loginEditTextPassword.setError("Minimum length of password should be 6");
             loginEditTextPassword.requestFocus();
@@ -67,38 +81,51 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         LoginProgressBar.setVisibility(View.VISIBLE);
 
+        /*
+        This code uses the entry point for Firebase and uses the signInWithEmailAndPassword method
+        to log in a user which is already created in Firebase. For now it redirects to the ProfileActivity,
+        this we can just edit later.
+         */
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 LoginProgressBar.setVisibility(View.GONE);
+
+                //If Email and password is correct, Login the user and redirect to ProfileActivity
                 if (task.isSuccessful()) {
                 Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                //This method ensures that the 2 activities Login and Signup gets removed from the stack
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
+
                 } else {
                     Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
 
+    /*
+       This method onClick makes sure that when you click on the buttons,
+       they do what they are supposed to do.
+     */
     @Override
     public void onClick(View view) {
 
         switch(view.getId()) {
+
+            //When button "Sign up" is pressed, redirect to SignUpActivity
             case R.id.buttonSignUpId:
 
-                startActivity(new Intent(this, SignupActivity.class));
-
+                startActivity(new Intent(this, SignUpActivity.class));
                 break;
 
+            //When button Login is pressed, call the method userLogin
             case R.id.buttonLoginId:
 
                 userLogin();
                 break;
 
         }
-
     }
 }
--- app/src/main/java/com/example/Booga/SignUpActivity.java
+++ app/src/main/java/com/example/Booga/SignUpActivity.java
@@ -20,12 +20,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
-public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     EditText signUpEditTextEmail, signUpEditTextPassword;
     Button buttonSignUp, buttonLogin;
     ProgressBar signUpProgressBar;
+
+    //This is the entry point for the Firebase Authentication SDK
     private FirebaseAuth mAuth;
 
     @Override
@@ -52,28 +54,40 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+
+        /*
+        This method Registers the user.
+         */
     private void registerUser() {
+
+        /*
+        This gets the text written by the user in the EditTextFields email and password.
+         */
         String email = signUpEditTextEmail.getText().toString().trim();
         String password = signUpEditTextPassword.getText().toString().trim();
 
+        // If the Email EditTextField is empty show an error and point where the error is by using .requestFocus
         if (email.isEmpty()) {
             signUpEditTextEmail.setError("Email is required");
             signUpEditTextEmail.requestFocus();
             return;
         }
-
+        /*
+         If the Email in the EditTextField does not match with an already registered Email in Firebase,
+         show an error and point where the error is by using .requestFocus
+         */
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             signUpEditTextEmail.setError("Please enter a valid email");
             signUpEditTextEmail.requestFocus();
             return;
         }
-
+        // If the Password EditTextField is empty show an error and point where the error is by using .requestFocus
         if(password.isEmpty()) {
             signUpEditTextPassword.setError("Password is required");
             signUpEditTextPassword.requestFocus();
             return;
         }
-
+        // If the Password EditTextField is not long enough, show an error and point where the error is by using .requestFocus
         if(password.length() < 6) {
             signUpEditTextPassword.setError("Minimum length of password should be 6");
             signUpEditTextPassword.requestFocus();
@@ -82,16 +96,25 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         signUpProgressBar.setVisibility(View.VISIBLE);
 
+
+        /*
+        This code uses the entry point for Firebase and uses the createUserWithEmailAndPassword method
+        to create a user so it is saved in Firebase. For now it redirects to the ProfileActivity,
+        this we can just edit later.
+         */
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 signUpProgressBar.setVisibility(View.GONE);
                 if(task.isSuccessful()) {
-                    Intent intent = new Intent(SignupActivity.this, ProfileActivity.class);
+                    //If everything succeeds, redirect to ProfileActivity
+                    Intent intent = new Intent(SignUpActivity.this, ProfileActivity.class);
+                    //This method ensures that the 2 activities Login and Signup gets removed from the stack
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
-
                     Toast.makeText(getApplicationContext(), "User registered successfully", Toast.LENGTH_SHORT).show();
+
+                    //If User is already registered, handle CollisionException and show a Toast message
                 } else {
                     if (task.getException() instanceof FirebaseAuthUserCollisionException) {
 
@@ -105,14 +128,21 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         });
     }
 
+
+    /*
+    This method onClick makes sure that when you click on the buttons,
+    they do what they are supposed to do.
+     */
     @Override
     public void onClick(View view) {
 
         switch(view.getId()) {
+            //When Sign up button is pressed, call the method registerUser
             case R.id.buttonSignUpSignUpId:
                 registerUser();
                 break;
 
+            //When button "Already have a login?" is pressed, redirect to LoginActivity
             case R.id.buttonSignUpLoginId:
                 startActivity(new Intent(this, LoginActivity.class));
 
